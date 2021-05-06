62659d342eb2d1d7e8999d602cd519f276b1aef4
fabiano ventura <fabianovguimaraes@hotmail.com>
2021-05-06 20:39:39 -0300

    Project created

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+/*declarando uma função */
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <div className="container">
+        <h1 className="text-primary">Olá mundo!</h1>
+         <DataTable/>
+
+      </div>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+/*Footer= Rodapé da pagina */
+
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://github.com/Fabiano-1979" target="_blank" rel="noreferrer">Fabiano Ventura Guimarães</a></p>
+        <p className="text-light"><small><strong>Semana Spring React</strong><br />
+          Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
