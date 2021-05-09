af87a96d4053e13eebe6d378d13972fbc871d26d
fabiano ventura <fabianovguimaraes@hotmail.com>
2021-05-09 17:35:11 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,16 +1,17 @@
-/*NavBAr , seria o componante do cabeçalho da pagina, configuração */
-
 import ImgDsDark from 'assets/img/ds_dark.svg';
-const NavBar = () =>{
-    return (
-        <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
-         <div className="container">
-          <nav className="my-2 my-md-0 mr-md-3">
+import { Link } from 'react-router-dom';
+const NavBar = () => {
+  return (
+    <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
+      <div className="container">
+        <nav className="my-2 my-md-0 mr-md-3">
+          <Link to="/">
             <img src={ImgDsDark} alt="DevSuperior" width="120" />
-          </nav>
-        </div>
-       </div>
-    );
-  }
-  
-  export default NavBar;
+          </Link>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
